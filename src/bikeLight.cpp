701aa952bb7a26e7501b3d46eafc9768d774b318
Danie Lopez Garcia <dalogax@gmail.com>
2020-08-11 21:06:23 +0200

    first commit

---
+++ src/bikeLight.cpp
@@ -0,0 +1,51 @@
+
+
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
+#define PIN        D5
+#define SWITCH     D6
+#define NUMPIXELS  20
+
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+uint32_t PINK = pixels.Color(255, 0, 64);
+uint32_t RED = pixels.Color(255, 0, 0);
+
+int brightness = 255;
+byte pink = LOW;
+
+void updatePixels(){
+
+  for(int i=0; i<NUMPIXELS; i++) {
+    if (pink == LOW){
+      pixels.setPixelColor(i, RED);
+    } else {
+      pixels.setPixelColor(i, PINK);
+    }
+  }
+  pixels.setBrightness(brightness);
+  pixels.show();
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(SWITCH, INPUT_PULLUP);
+
+  pixels.begin();
+  pixels.clear();
+}
+
+void loop() {
+
+  pink = digitalRead(SWITCH);
+
+  if (brightness > 2) {
+    --brightness;
+  } else {
+    brightness = 255;
+  }
+  updatePixels();
+  delay(5);
+}
